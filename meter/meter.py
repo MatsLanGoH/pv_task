bf9d1f559bca92984db30d5bf8220cc89ecbd4a7
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2020-03-08 12:12:36 +0900

    Initial docker configuration
    - layout and basic folder structure was taken from
    https://github.com/mobilityhouse/task

---
+++ meter/meter.py
@@ -0,0 +1,17 @@
+"""
+Meter simulator
+"""
+import time
+
+
+def main():
+    try:
+        while True:
+            print("Meter simulator...")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ pv_sim/pv.py
@@ -0,0 +1,17 @@
+"""
+PV Simulator
+"""
+import time
+
+
+def main():
+    try:
+        while True:
+            print("PV Simulator...")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
