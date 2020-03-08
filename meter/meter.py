ecd8914b6869d0fd9fd8d73e8da4f99b2be2a5f4
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2020-03-08 21:50:51 +0900

    Add dev-requires

--- meter/meter.py
+++ meter/meter.py
@@ -1,13 +1,41 @@
 """
 Meter simulator
 """
+import random
+
 import time
+import pika
+
+
+class BrokerSender:
+    def __init__(self, broker_host: str, queue: str):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=broker_host)
+        )
+        self.channel = self.connection.channel()
+        self.queue = self.channel.queue_declare(queue=queue, durable=True)
+
+
+class Meter:
+    @staticmethod
+    def generate_value():
+        return random.randint(0, 9000)
 
 
 def main():
+    QUEUE = "task_queue"
+    sender = BrokerSender(broker_host="host.docker.internal", queue=QUEUE)
+    meter = Meter()
     try:
         while True:
-            print("Meter simulator...")
+            msg = meter.generate_value()
+            sender.channel.basic_publish(
+                exchange="",
+                routing_key=QUEUE,
+                body=msg,
+                properties=pika.BasicProperties(delivery_mode=2),
+            )
+            print(f" [x] Sent {msg} watts.")
             time.sleep(1)
     except KeyboardInterrupt:
         pass
